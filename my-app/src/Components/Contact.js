7d79067a3dc5e582075ce488411533a997215702
Meghanath91 <meghanath.balaji@gmail.com>
2020-04-09 03:41:12 -0400

    styles fixed

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -87,7 +87,7 @@ class Contact extends Component {
 					   </p>
 				   </div>
 
-               <div className="widget widget_tweets">
+               {/* <div className="widget widget_tweets">
                   <h4 className="widget-title">Latest Tweets</h4>
                   <ul id="twitter">
                      <li>
@@ -107,7 +107,7 @@ class Contact extends Component {
                         <b><a href="#">3 Days Ago</a></b>
                      </li>
                   </ul>
-		         </div>
+		         </div> */}
             </aside>
       </div>
    </section>
