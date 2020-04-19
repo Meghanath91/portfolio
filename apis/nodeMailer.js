2e13888e67abe3e1f7e49dddb9c65c0b0bee99eb
Meghanath91 <meghanath.balaji@gmail.com>
2020-04-19 19:09:09 -0400

    emails changed

--- apis/nodeMailer.js
+++ apis/nodeMailer.js
@@ -7,34 +7,23 @@ In Auth object , we specify our email and password
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "meghanath.balaji@gmail.com", //replace with your email
-      pass: "SANju9495*", //replace with your password
+      user: "meghanath.ponoly@gmail.com", //replace with your email
+      pass: "VARsha9495*", //replace with your password
     },
   });
 
   const adminMail =`
-  <style>
-  .content {
-    background-image:url("https://images.unsplash.com/photo-1509641498745-13c26fd1ed89?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60");
-    height:500px;
-  }
-  </style>
-  <div class="content">
   <h2>Contact details</h2>
   <h5> name:${msg.fullName} </h5><br>
   <h5> subject: ${msg.Subject} </h5>
   <p>${msg.Message}</p>
-  </div>
   ` ;
 
   const clientMail = `
-
   <h4>Thank you ${msg.fullName} for reaching out !</h4><br>
-  <img src="https://images.unsplash.com/photo-1509641498745-13c26fd1ed89?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
   <p> I will get back to you within 1-2 business days. You can also reach out to me on +1 647-528-7504 for urgent enquiries.</p>
   <br>
   <p>Regards,</p>
-  
   <h5>Meghanath </h5>
   `
 
@@ -46,7 +35,7 @@ In Auth object , we specify our email and password
   };
 
   const mailAdmin = {
-    from: "meghanath.balaji@gmail.com", //replace with your email
+    from: "meghanath.ponoly@gmail.com", //replace with your email
     to: "meghanath.balaji@gmail.com", //replace with your email
     subject: msg.Subject,
     html: adminMail
