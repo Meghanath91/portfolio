ba1747455dbbcf1a6d61e1303ae4b5c4385a2ebd
Meghanath91 <meghanath.balaji@gmail.com>
2020-04-20 17:17:28 -0400

    server changed

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const dev = app.get("env") !== "production";
+// const dev = app.get("env") !== "production";
 const nodeMailer = require("./apis/nodeMailer");
 const server = createServer(app);
 
 app.use(cors({ origin: true, credentials: true }));
-if (!dev) {
+// if (!dev) {
   app.disable("x-powered-by");
   app.use(compression());
   app.use(morgan("common"));
@@ -27,7 +27,7 @@ if (!dev) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   
-  app.post("/contact", (req, res) => {
+  app.post("api/contact", (req, res) => {
     const msgInfo=req.body;
     nodeMailer(msgInfo)
     console.log("email sent");
@@ -37,23 +37,22 @@ if (!dev) {
     if (err) throw err;
     console.log("Server started");
   });
-}
+// }
 
-if (dev) {
-  app.use(morgan("dev"));
+// if (dev) {
+//   app.use(morgan("dev"));
+// }
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.post("/api/contact", (req, res) => {
-  const msgInfo=req.body;
-  nodeMailer(msgInfo)
-  console.log("email sent");
-  res.json("success");
-});
+// app.post("/api/contact", (req, res) => {
+//   const msgInfo=req.body;
+//   nodeMailer(msgInfo)
+//   console.log("email sent");
+//   res.json("success");
+// });
 
 server.listen(PORT, (err) => {
   if (err) throw err;
   console.log("Server started");
 });
-}
