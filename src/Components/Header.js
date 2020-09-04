5a0f751811960d17d61fae9281d899abb53669b8
Meghanath91 <meghanath.balaji@gmail.com>
2020-09-04 17:57:27 -0400

    text edit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -73,7 +73,7 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm Meghanath.</h1>
+            <h1 className="responsive-headline">I'm Meghanath</h1>
 
             <h3>
               I'm a {city} based <span>{occupation}</span>. {description}.
